e2c0a11e055c2e525a3caf4c4e7649d22aa3e39f
Rellika Kisyula <kisyular@msu.edu>
2017-01-31 16:24:41 -0500

    Improved the JS as Instructor suggested

--- js/script.js
+++ js/script.js
@@ -1,4 +1,95 @@
-var playAudio = function (idName) {
-    document.getElementById(idName).play();
-    document.getElementById(idName).play();
+/*var playAudio = function(idName) {
+  document.getElementById(idName).play();
+  document.getElementById(idName).play();
 }
+*/
+
+var boom = document.getElementById("boom");
+boom.addEventListener('click', function() {
+  var audio = new Audio('sounds/boom.wav');
+  audio.play();
+});
+boom.addEventListener('mouseenter', function() {
+  var audio = new Audio('sounds/boom.wav');
+  audio.play();
+});
+
+var clap = document.getElementById("clap");
+clap.addEventListener('click', function() {
+  var audio = new Audio('sounds/clap.wav');
+  audio.play();
+});
+clap.addEventListener('mouseenter', function() {
+  var audio = new Audio('sounds/clap.wav');
+  audio.play();
+});
+
+var hihat = document.getElementById("hihat");
+hihat.addEventListener('click', function() {
+  var audio = new Audio('sounds/hihat.wav');
+  audio.play();
+});
+hihat.addEventListener('mouseenter', function() {
+  var audio = new Audio('sounds/hihat.wav');
+  audio.play();
+});
+
+var kick = document.getElementById("kick");
+kick.addEventListener('click', function() {
+  var audio = new Audio('sounds/kick.wav');
+  audio.play();
+});
+kick.addEventListener('mouseenter', function() {
+  var audio = new Audio('sounds/kick.wav');
+  audio.play();
+});
+
+var openhat = document.getElementById("openhat");
+openhat.addEventListener('click', function() {
+  var audio = new Audio('sounds/openhat.wav');
+  audio.play();
+});
+openhat.addEventListener('mouseenter', function() {
+  var audio = new Audio('sounds/openhat.wav');
+  audio.play();
+});
+
+var snare = document.getElementById("snare");
+snare.addEventListener('click', function() {
+  var audio = new Audio('sounds/snare.wav');
+  audio.play();
+});
+snare.addEventListener('mouseenter', function() {
+  var audio = new Audio('sounds/snare.wav');
+  audio.play();
+});
+
+var tink = document.getElementById("tink");
+tink.addEventListener('click', function() {
+  var audio = new Audio('sounds/tink.wav');
+  audio.play();
+});
+tink.addEventListener('mouseenter', function() {
+  var audio = new Audio('sounds/tink.wav');
+  audio.play();
+});
+
+var tom = document.getElementById("tom");
+tom.addEventListener('click', function() {
+  var audio = new Audio('sounds/tom.wav');
+  audio.play();
+});
+tom.addEventListener('mouseenter', function() {
+  var audio = new Audio('sounds/tom.wav');
+  audio.play();
+});
+
+var ride = document.getElementById("ride");
+ride.addEventListener('click', function() {
+  var audio = new Audio('sounds/ride.wav');
+  audio.play();
+});
+ride.addEventListener('mouseenter', function() {
+  var audio = new Audio('sounds/ride.wav');
+  audio.play();
+});

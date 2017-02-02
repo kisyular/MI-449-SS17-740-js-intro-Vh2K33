57c91bffddd87391373545d91f65a38371010adf
Rellika Kisyula <kisyular@msu.edu>
2017-02-01 23:31:38 -0500

    Changed the code to a shorter one as based on Suggestion from Prof Chris

--- js/script.js
+++ js/script.js
@@ -1,95 +1,18 @@
-/*var playAudio = function(idName) {
-  document.getElementById(idName).play();
-  document.getElementById(idName).play();
+var attachSoundToButton = function (soundName) {
+    var button = document.getElementById(soundName)
+    var audio = new Audio('sounds/' + soundName + '.wav')
+    var playSound = function () {
+        audio.play()
+    }
+    button.addEventListener('click', playSound)
+    button.addEventListener('mouseenter', playSound)
 }
-*/
-
-var boom = document.getElementById("boom");
-boom.addEventListener('click', function() {
-  var audio = new Audio('sounds/boom.wav');
-  audio.play();
-});
-boom.addEventListener('mouseenter', function() {
-  var audio = new Audio('sounds/boom.wav');
-  audio.play();
-});
-
-var clap = document.getElementById("clap");
-clap.addEventListener('click', function() {
-  var audio = new Audio('sounds/clap.wav');
-  audio.play();
-});
-clap.addEventListener('mouseenter', function() {
-  var audio = new Audio('sounds/clap.wav');
-  audio.play();
-});
-
-var hihat = document.getElementById("hihat");
-hihat.addEventListener('click', function() {
-  var audio = new Audio('sounds/hihat.wav');
-  audio.play();
-});
-hihat.addEventListener('mouseenter', function() {
-  var audio = new Audio('sounds/hihat.wav');
-  audio.play();
-});
-
-var kick = document.getElementById("kick");
-kick.addEventListener('click', function() {
-  var audio = new Audio('sounds/kick.wav');
-  audio.play();
-});
-kick.addEventListener('mouseenter', function() {
-  var audio = new Audio('sounds/kick.wav');
-  audio.play();
-});
-
-var openhat = document.getElementById("openhat");
-openhat.addEventListener('click', function() {
-  var audio = new Audio('sounds/openhat.wav');
-  audio.play();
-});
-openhat.addEventListener('mouseenter', function() {
-  var audio = new Audio('sounds/openhat.wav');
-  audio.play();
-});
-
-var snare = document.getElementById("snare");
-snare.addEventListener('click', function() {
-  var audio = new Audio('sounds/snare.wav');
-  audio.play();
-});
-snare.addEventListener('mouseenter', function() {
-  var audio = new Audio('sounds/snare.wav');
-  audio.play();
-});
-
-var tink = document.getElementById("tink");
-tink.addEventListener('click', function() {
-  var audio = new Audio('sounds/tink.wav');
-  audio.play();
-});
-tink.addEventListener('mouseenter', function() {
-  var audio = new Audio('sounds/tink.wav');
-  audio.play();
-});
-
-var tom = document.getElementById("tom");
-tom.addEventListener('click', function() {
-  var audio = new Audio('sounds/tom.wav');
-  audio.play();
-});
-tom.addEventListener('mouseenter', function() {
-  var audio = new Audio('sounds/tom.wav');
-  audio.play();
-});
-
-var ride = document.getElementById("ride");
-ride.addEventListener('click', function() {
-  var audio = new Audio('sounds/ride.wav');
-  audio.play();
-});
-ride.addEventListener('mouseenter', function() {
-  var audio = new Audio('sounds/ride.wav');
-  audio.play();
-});
+attachSoundToButton('boom')
+attachSoundToButton('clap')
+attachSoundToButton('hihat')
+attachSoundToButton('kick')
+attachSoundToButton('openhat')
+attachSoundToButton('snare')
+attachSoundToButton('tink')
+attachSoundToButton('tom')
+attachSoundToButton('ride')
